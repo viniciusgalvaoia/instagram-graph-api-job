ff358e26ab1282057473b5413100f82f7181cd32
unknown <viniciusgalvaoia@gmail.com>
2021-04-02 19:32:03 -0300

    fixing docstring of main

--- job/__main__.py
+++ job/__main__.py
@@ -1,19 +1,9 @@
 """
-Main module of the monthly-bi-python-job project. ETL to create the MaterialRevenue table:
+Main module of the instagram-graph-api-job-job project.
 
-*   format_query formats the query, adding the year and month from the date
-*   execute_query, from utils, executes the query in athena and stores the table in a dataframe
-*   format_folder_path formats the folder path where table will be saved
-*   write_to_s3 sends the dataframe as parquet file to formatted bucket at s3
 """
 
 import logging
-import os
-from datetime import datetime
-
-import awswrangler as wr
-import boto3
-import pandas as pd
 from rich.logging import RichHandler
 from rich.traceback import install
 
@@ -30,7 +20,6 @@ logger = logging.getLogger("main")
 
 
 if __name__ == "__main__":
-    # logging info
-    logger.info(f"date: ")
+    logger.info("STARTING JOB")
 
     logger.info("END OF JOB")
